cc1a6c09da96916e3467fc497544066fd633a798
shehan C shanaka <39334423+shehancshanaka@users.noreply.github.com>
2021-09-08 18:56:48 +0530

    tweak NavBar Styling

--- library_management_system_mern/src/App.js
+++ library_management_system_mern/src/App.js
@@ -1,31 +1,36 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
-import { Header, Main, Footer } from "./components/Layout"
-import {NavBar,NavItem,NavLink} from './components/navbar'
+import { BsBookHalf } from "react-icons/bs";
+import { Main, Footer } from "./components/Layout";
+import { NavBar, NavItem, NavLink } from "./components/navbar";
 function App() {
   const theme = {
     primary: {
       main: "#29b6f6",
       light: "#73e8ff",
       dark: "# 0086c3",
-      textColor : "#000"
+      textColor: "#000",
     },
     secondary: {
-      main: "#fff"
+      main: "#fff",
     },
   };
   return (
-    <ThemeProvider theme = {theme}>
-      <Header>
-        <NavBar>
-          <NavItem>
-            <NavLink href='#'>Catalog</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href='#'>Dashboard</NavLink>
-          </NavItem>
-        </NavBar>
-      </Header>
+    <ThemeProvider theme={theme}>
+      <NavBar>
+        <NavItem>
+          <NavLink href="#">
+            <BsBookHalf></BsBookHalf>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Catalog</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Dashboard</NavLink>
+        </NavItem>
+      </NavBar>
+
       <Main>This is Main</Main>
       <Footer>This is Footer</Footer>
     </ThemeProvider>
--- library_management_system_mern/src/components/Layout.js
+++ library_management_system_mern/src/components/Layout.js
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-export const Header = styled.header`
-  height: 5vh;
-`;
+
 export const Main = styled.main`
-  height: 95vh;
+min-height :100%
+  padding : 2.5vh
 `;
 
 export const Footer = styled.footer`
